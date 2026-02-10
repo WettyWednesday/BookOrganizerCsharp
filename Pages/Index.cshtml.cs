9bb01b18be226b1349983ab05e3056d273eec30b
WettyWednesday <magnusven2@gmail.com>
2026-02-10 10:42:32 +0100

    Vscode recommended Razor setup

---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BookOrganizer.Pages;
+
+public class IndexModel : PageModel
+{
+    public void OnGet()
+    {
+
+    }
+}
