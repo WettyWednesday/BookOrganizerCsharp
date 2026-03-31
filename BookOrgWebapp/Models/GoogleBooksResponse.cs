a2d682b5b96019f1a18902d9470cccfbdee16f87
Patrick Machalet <s203932@dtu.dk>
2026-03-31 15:31:29 +0200

    Quick update, still work in progress

--- BookOrgWebapp/Models/GoogleBooksResponse.cs
+++ BookOrgWebapp/Models/GoogleBooksResponse.cs
@@ -13,4 +13,5 @@ public class VolumeInfo
     public string? Title { get; set; }
     public List<string>? Authors { get; set; }
     public string? Description { get; set; }
+    public string? Publisher { get; set; }
 }
