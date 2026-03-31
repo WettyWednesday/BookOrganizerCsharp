5464db3b13f19956eaf21cec2bf5c5ff70b8dc94
Patrick Machalet <s203932@dtu.dk>
2026-03-31 16:23:38 +0200

    Key info to be stored

--- BookOrgWebapp/Models/GoogleBooksResponse.cs
+++ BookOrgWebapp/Models/GoogleBooksResponse.cs
@@ -11,7 +11,20 @@ public class Item
 public class VolumeInfo
 {
     public string? Title { get; set; }
+    public string? Subtitle { get; set; }
     public List<string>? Authors { get; set; }
     public string? Description { get; set; }
     public string? Publisher { get; set; }
+    public string? PublishedDate { get; set; }
+    public List<string>? Categories { get; set; }
+    public string? Language { get; set; }
+    public int? PageCount { get; set; }
+
+    public ImageLinks? ImageLinks { get; set; } 
+}
+
+public class ImageLinks
+{
+    public string? SmallThumbnail { get; set; }
+    public string? thumbnail { get; set; }
 }
